ed04f8a0b132c6e4bef42a7d58a2d9cd16a7d588
Elk1v <mateyilkiv@gmail.com>
2020-06-23 15:48:52 +0200

    Перепишет компонент App

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,23 +1,44 @@
-import React from "react";
+import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
-
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import Main from "../main/main.jsx";
+import Property from "../property/property.jsx";
 
 const titleClickHandler = () => {
   return null;
 };
 
-const App = (props) => {
-  const {offers, offersCount} = props;
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
 
-  return (
-    <Main
-      offers={offers}
-      offersCount={offersCount}
-      onTitleClick={titleClickHandler}
-    />
-  );
-};
+  _renderMain() {
+    const {offers, offersCount} = this.props;
+    return (
+      <Main
+        offers={offers}
+        offersCount={offersCount}
+        onTitleClick={titleClickHandler}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <BrowserRouter> 
+        <Switch>
+          <Route exact path="/">
+            {this._renderMain()}
+          </Route>
+          <Route exact path="/property">
+            <Property />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
 
 App.propTypes = {
   offers: PropTypes.array.isRequired,
